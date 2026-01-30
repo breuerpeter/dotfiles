3761eb92863a9fd80d457a6f89ef9c835504e83f
Peter Breuer <peter.breuer.profiles@gmail.com>
2026-01-30 09:45:40 +0100

    chore: remove PX4 specific stuff

--- setup.sh
+++ setup.sh
@@ -6,17 +6,6 @@ DOTFILES_DIR="$(cd "$(dirname "$0")" && pwd)"
 # Wezterm
 ln -sf "$DOTFILES_DIR/wezterm" ~/.config/wezterm
 
-# Bash snippets
-SOURCE_LINE="source $DOTFILES_DIR/bash/px4.sh"
-if ! grep -qF "$SOURCE_LINE" ~/.bashrc 2>/dev/null; then
-    echo "" >> ~/.bashrc
-    echo "# Dotfiles" >> ~/.bashrc
-    echo "$SOURCE_LINE" >> ~/.bashrc
-    echo "Added bash snippets to ~/.bashrc"
-else
-    echo "Bash snippets already in ~/.bashrc"
-fi
-
 # Window Calls GNOME extension (for window manipulation on Wayland)
 EXTENSION_UUID="window-calls@domandoman.xyz"
 EXTENSION_DIR="$HOME/.local/share/gnome-shell/extensions/$EXTENSION_UUID"
